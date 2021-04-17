a4be97019d436f15746bf722e2ec43e4d3e04d41
caitlinpmarshall <caitlinpmarshall@gmail.com>
2021-04-17 16:31:46 -0400

    Removed unused plugins and themes; fixed missing home item from mobile view menu

--- wp-content/plugins/so-widgets-bundle/widgets/editor/tpl/default.php
+++
@@ -1,5 +0,0 @@
-<?php if( !empty( $instance['title'] ) ) echo $args['before_title'] . $instance['title'] . $args['after_title'] ?>
-
-<div class="siteorigin-widget-tinymce textwidget">
-	<?php echo $text ?>
-</div>
--- wp-content/plugins/themeisle-companion/vendor/codeinwp/themeisle-content-forms/load.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Loader for the ThemeIsle\ContentForms feature
- *
- * @package     ThemeIsle\ContentForms
- */
-
-require_once 'form_manager.php';
