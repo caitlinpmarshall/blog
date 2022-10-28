42c76c0197f9341a1c17eedb771f3bfd65876ef0
caitlinpmarshall <caitlinpmarshall@gmail.com>
2022-10-28 15:25:39 -0400

    Starts formatting api display page template

--- wp-content/themes/astra-child/templates/api-display.php
+++ wp-content/themes/astra-child/templates/api-display.php
@@ -14,26 +14,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 get_header(); ?>
 
-<?php if ( astra_page_layout() == 'left-sidebar' ) : ?>
-
-	<?php get_sidebar(); ?>
-
-<?php endif ?>
-
 	<div id="primary" <?php astra_primary_class(); ?>>
+		<h2>howdy</h2>
 
-		<?php astra_primary_content_top(); ?>
+	</div>
 
-		<?php astra_content_page_loop(); ?>
-
-		<?php astra_primary_content_bottom(); ?>
-
-	</div><!-- #primary -->
-
-<?php if ( astra_page_layout() == 'right-sidebar' ) : ?>
-
-	<?php get_sidebar(); ?>
-
-<?php endif ?>
 
 <?php get_footer(); ?>
--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"HNt_jKc%9._8T0G4?yg?M%::At `3h,Bi)$PsIfgjvg9y=c,jvJ?Rz;JB2WO_R.2";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:474;}
