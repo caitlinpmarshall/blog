d7a404e85897050f1ba09f093e20b9bfef92fc30
caitlinpmarshall <caitlinpmarshall@gmail.com>
2022-11-02 09:59:57 -0400

    Begins customizing featured org acf template and fields

--- wp-content/themes/astra-child/templates/featured-org.php
+++ wp-content/themes/astra-child/templates/featured-org.php
@@ -10,9 +10,11 @@
 
 $id = 'about-' . $block['id'];
 $block['className'] = "acf-common-block";
-$block_image = get_field('block_image');
-$block_heading = get_field('block_heading');
-$block_description = get_field('block_description');
+$hero_image = get_field('hero_image');
+$organization_name = get_field('organization_name');
+$website_link = get_field('website_link');
+$organization_description = get_field('organization_description');
+
 
 get_header();
 ?>
@@ -21,18 +23,18 @@ get_header();
 	<div class="container">
 		<div class="d-flex align-items-center">
 			<?php
-			if (!empty($block_image)) { ?>
+			if (!empty($hero_image)) { ?>
 				<figure class="about-hero-img w-100">
-					<img src="<?php echo $block_image; ?>" alt="<?php echo $block_image['alt']; ?>" class="w-100">
+					<img src="<?php echo $hero_image; ?>" alt="<?php echo $hero_image['alt']; ?>" class="w-100">
 				</figure>
 			<?php } ?>
 
 			<div class="about-description">
-				<?php if (!empty($block_heading)) { ?>
-					<h2><?php echo $block_heading; ?></h2>
+				<?php if (!empty($organization_name)) { ?>
+					<h2><?php echo $organization_name; ?></h2>
 				<?php }
-				if (!empty($block_description)) { ?>
-					<?php echo $block_description; ?>
+				if (!empty($organization_description)) { ?>
+					<?php echo $organization_description; ?>
 				<?php } ?>
 			 </div>
 		</div>
